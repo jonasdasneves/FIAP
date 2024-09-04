d6c6407506f67eda75e10b427af61482ffe721af
jonasdasneves <jonascamp2004@gmail.com>
2024-09-03 22:14:20 -0300

    aula3

--- exemplos_iniciais/programas_python_aula_03/eleicao.py
+++ exemplos_iniciais/programas_python_aula_03/eleicao.py
@@ -1,6 +1,3 @@
-#Pergunta quantas pessoas há na cidade
-pessoas = int(input('Quantas pessoas há na cidade?'))
-
 #Votos em branco
 branco = int(input('Quantos votos em branco foram contados?'))
 #Votos nulos
@@ -9,6 +6,7 @@ nulo = int(input('Quantos votos nulos foram contados?'))
 validos = int(input('Quantos votos válidos foram contados?'))
 
 #Calcula e exibe a porcentagem de votos
-print(f'{(branco/pessoas)*100}% das pessoas votaram em branco')
-print(f'{(nulo/pessoas)*100}% das pessoas votaram nulo')
-print(f'{(validos/pessoas)*100}% das pessoas votaram em algum candidato')
+pessoas = branco+nulo+validos
+print(f'{((branco/pessoas)*100):.2f}% das pessoas votaram em branco')
+print(f'{((nulo/pessoas)*100):.2f}% das pessoas votaram nulo')
+print(f'{((validos/pessoas)*100):.2f}% das pessoas votaram em algum candidato')
