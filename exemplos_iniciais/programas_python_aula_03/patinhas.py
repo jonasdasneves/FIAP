b90ea7fce5f10c95917062317180bbd96af7a044
jonasdasneves <jonascamp2004@gmail.com>
2024-09-03 21:40:58 -0300

    aula3

--- exemplos_iniciais/programas_python_aula_03/patinhas.py
+++ exemplos_iniciais/programas_python_aula_03/patinhas.py
@@ -1,7 +1,7 @@
 #A fortuna do tio patinhas é 65.5 bilhões
-fortuna = 65400000000.00
+fortuna = 65.4
 
 #A divisão foi feita da seguinte forma:
-print(f'O orfanato dos patinhos recebeu {fortuna*0.15} dólares, sendo 15% do total')
-print(f'Donald recebeu {fortuna*0.25} dólares, sendo 25% do total')
-print(f'O restante foi dividido para os 3 sobrinhos-netos. Cada um deles recebeu {((fortuna*0.6)/3):.2f} dólares')
+print(f'O orfanato dos patinhos recebeu {fortuna*0.15} bilhões, sendo 15% do total')
+print(f'Donald recebeu {fortuna*0.25} bilhões, sendo 25% do total')
+print(f'O restante foi dividido para os 3 sobrinhos-netos. Cada um deles recebeu {((fortuna*0.6)/3):.2f} bilhões')
